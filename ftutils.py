3343cf66d8cee4c499fc2b6caef3befd455f7b98
ntpdev <52379467+ntpdev@users.noreply.github.com>
2025-03-12 09:54:35 +0000

    updates

--- ftutils.py
+++ ftutils.py
@@ -3,6 +3,7 @@ import re
 from datetime import datetime
 from pathlib import Path
 from collections.abc import Callable
+from typing import Any
 
 import requests
 from bs4 import BeautifulSoup
@@ -16,7 +17,17 @@ from rich.table import Table
 
 from htm2md import html_to_markdown
 
+NYT_URL = "https://www.nytimes.com/"
+WSJ_URL = "https://www-wsj-com.translate.goog/?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp&_x_tr_hist=true"
+
 console = Console()
+
+
+class ErrorInfo(BaseModel):
+    error: bool
+    type: str
+    message: str
+    url: str
 
 
 class ArticleLink(BaseModel):
@@ -45,18 +56,17 @@ class ArticleList(BaseModel):
 def merge(*article_lists: ArticleList) -> ArticleList:
     """Merge article lists together, merging duplicate URLs."""
     # Create a dictionary to track URLs that have already been added
-    seen = {}
+    merged = {}
     for article_list in article_lists:
         for article in article_list.articles:
-            if article.url in seen:
+            if article.url in merged:
                 # Replace if the summary in the current article is longer
-                if len(article.summary) > len(seen[article.url].summary):
-                    seen[article.url] = article
+                if len(article.summary) > len(merged[article.url].summary):
+                    merged[article.url] = article
             else:
-                seen[article.url] = article
+                merged[article.url] = article
 
-    merged = ArticleList(timestamp_retrieved=article_lists[0].timestamp_retrieved, source=article_lists[0].source, articles=list(seen.values()))
-    return merged
+    return ArticleList(timestamp_retrieved=article_lists[0].timestamp_retrieved, source=article_lists[0].source, articles=list(merged.values()))
 
 
 class Citation(BaseModel):
@@ -101,16 +111,17 @@ retrieve_stock_quotes_fn = {
 }
 
 
-def fn_name(d):
-    return d["function"]["name"]
-
-
 @cache
-def ftutils_functions():
+def ftutils_functions() -> dict[str, dict[str, Any]]:
+    """Returns a dictionary mapping function names to their definitions and a callable."""
+
+    def name(d):
+        return d["function"]["name"]
+
     return {
-        fn_name(retrieve_headlines_fn): {"defn": retrieve_headlines_fn, "fn": retrieve_headlines},
-        fn_name(retrieve_article_fn): {"defn": retrieve_article_fn, "fn": retrieve_article},
-        fn_name(retrieve_stock_quotes_fn): {"defn": retrieve_stock_quotes_fn, "fn": retrieve_stock_quotes},
+        name(retrieve_headlines_fn): {"defn": retrieve_headlines_fn, "fn": retrieve_headlines},
+        name(retrieve_article_fn): {"defn": retrieve_article_fn, "fn": retrieve_article},
+        name(retrieve_stock_quotes_fn): {"defn": retrieve_stock_quotes_fn, "fn": retrieve_stock_quotes},
     }
 
 
@@ -152,45 +163,84 @@ def add_citation(text: str, cite: Citation) -> str:
     return s + text
 
 
-def add_exchange_info(ticker: str) -> str:
-    t = ticker if ticker.find(":") >= 0 else ticker + ":UN"
-    return t.upper()
+def get_bnnbloomberg_quote(symbol: str) -> dict:
+    """
+    Fetches JNK:UN summary info and company info
+
+    Returns:
+        dict: The JSON response as a dictionary, or None if an error occurs.
+    """
+    result = {}
+
+    def merge_from(d):
+        for k, v in d.items():
+            if v is not None:
+                if isinstance(v, str):
+                    try:
+                        result[k] = float(v)
+                    except:
+                        result[k] = v
+                else:
+                    result[k] = v
+
+    try:
+        url = "https://bnn.stats.bellmedia.ca/bnn/api/stock/Scorecard?symbol=" + symbol
+        console.print(f"get quote scorecard from {url}")
+        response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+        scorecard = response.json()
+        url = "https://bnn.stats.bellmedia.ca/bnn/api/stock/companyInfo?brand=bnn&lang=en&symbol=" + symbol
+        response = requests.get(url)
+        response.raise_for_status()  # Raise HTTPError for bad responses (4xx or 5xx)
+        company_info = response.json()
+
+        merge_from(scorecard)
+        merge_from(company_info)
+    except requests.exceptions.RequestException as e:
+        print(f"Error during request: {e}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")  # catch any other errors
+    return result
 
 
-def process_bnn_stock_page(page, ticker: str) -> dict | None:
-    url = f"https://www.bnnbloomberg.ca/stock/{ticker}/"
-    console.print("navigating to {url}", style="yellow")
-    page.goto(url)
+def process_bnn_stock_page(page) -> dict | None:
+    # url = f"https://www.bnnbloomberg.ca/stock/{ticker}/"
+    # console.print("navigating to {url}", style="yellow")
+    # page.goto(url)
 
-    scorecard = page.locator(".bmw-scorecard.medium")
-    if not scorecard:
-        return None
-
-    print("scorecard found " + ticker)
     # the table is loaded on demand via a rest call when it is in view
     # alternatively use page.keyboard.press("PageDown")
-    scorecard.scroll_into_view_if_needed()
+    # <h2 id="bmw-scorecard--r86513c" class="bmw-header__title">Scorecard</h2>
+    # breakpoint()
     page.wait_for_timeout(1000)
+    # page.keyboard.press("PageDown")
+    # page.wait_for_timeout(125)
+    # page.get_by_text("Scorecard").scroll_into_view_if_needed(timeout=9000)
+
     ticker_head = page.locator("h1.c-heading")
     name = page.locator("h2.bmw-market-status__title")
     close = page.locator("span.bmw-market-status__info__price")
-    info = page.locator("section.bmw-company-info div.bmw-company-info__tab div.bmw-company-info__tab__content > p")
     d = {}
-    d["ticker"] = ticker_head.inner_text()
+    d["symbol"] = ticker_head.inner_text()
     d["name"] = name.inner_text()
-    d["description"] = info.inner_text()
     d["close"] = float(close.inner_text())
+    console.print("scorecard found " + d["symbol"], style="yellow")
     # Find all list items within the scorecard
-    listitems = page.locator("section.bmw-scorecard.medium > ul > li")
-    for item in listitems.all():
-        label = item.locator("span").inner_text().lower()
-        value = item.locator("strong").inner_text()
-        if value != "-":
-            try:
-                v = float(value)
-            except ValueError:
-                v = value
-            d[label] = v
+    # listitems = page.locator("section.bmw-scorecard > ul > li")
+    # for item in listitems.all():
+    #     label = item.locator("span").inner_text().lower()
+    #     value = item.locator("strong").inner_text()
+    #     if value != "-":
+    #         try:
+    #             v = float(value)
+    #         except ValueError:
+    #             v = value
+    #         d[label] = v
+
+    # page.get_by_text("Company Info").scroll_into_view_if_needed(timeout=9000)
+    # info = page.locator("section.bmw-company-info div.bmw-company-info__tab div.bmw-company-info__tab__content > p")
+    # d["description"] = info.inner_text()
+
     return d
 
 
@@ -264,32 +314,37 @@ def retrieve_wsj_article(url: str) -> str:
     save_markdown_article(cite.title, md)
     return md
 
-def retrieve_using_playwright(url_dict: dict[str, Callable], headless: bool = False) -> dict[str, any]:
+
+def retrieve_using_playwright(url_dict: dict[str, Callable], headless: bool = False) -> dict[str, Any]:
     """Generic wrapper for calling a parsing function on a web page."""
     results = {}
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=headless)
         iphone_15 = p.devices["iPhone 15"]
         context = browser.new_context(**iphone_15)
-        for url, parse_function in url_dict.items():
-            try:
-                page = context.new_page()
-                console.print(f"fetching {url}", style="yellow")
-                page.goto(url)
+        try:
+            page = context.new_page()
+            for url, parse_function in url_dict.items():
+                try:
+                    console.print(f"fetching {url}", style="yellow")
+                    page.goto(url)
+                    page.wait_for_load_state("domcontentloaded")
+                    # Call the parsing function for this page
+                    results[url] = parse_function(page)
 
-                # Call the parsing function for this page
-                results[url] = parse_function(page)
-
-            except Exception as e:
-                console.print(f"error retrieving {e}", style="red")
-                results[url] = None
-            finally:
-                browser.close()
-
+                except Exception as e:
+                    console.print(f"error retrieving {url}: {e}", style="red")  # Improved error message
+                    results[url] = ErrorInfo(error=True, type=type(e).__name__, message=str(e), url=url)
+            page.close()
+        finally:
+            browser.close()
+    pprint(results)
     return results
 
-def parse_nytimes_homepage(page) -> list[ArticleLink]:
+
+def parse_nyt_homepage(page) -> list[ArticleLink]:
     """retrieve first 10 headlines from nyt mobile home page"""
+
     def is_valid(text: str) -> bool:
         return text is not None and text != "LIVE" and "min read" not in text
 
@@ -299,27 +354,27 @@ def parse_nytimes_homepage(page) -> list[ArticleLink]:
         button.click()
 
     # accept compliance
-    if div := page.wait_for_selector('div#complianceOverlay', timeout=1000):
-        div.wait_for_selector('button', timeout=100).click()
+    if div := page.wait_for_selector("div#complianceOverlay", timeout=1000):
+        div.wait_for_selector("button", timeout=100).click()
 
-    if element:= page.locator('span[data-testid="todays-date"]'):
+    if element := page.locator('span[data-testid="todays-date"]'):
         date_string = element.inner_text()
         try:
             # Parse the date string, ignoring the day
-            pubdate = datetime.strptime(date_string, '%A, %B %d, %Y').date()
+            pubdate = datetime.strptime(date_string, "%A, %B %d, %Y").date()
             console.print(f"published {pubdate}", style="yellow")
         except ValueError:
             console.print(f"Error: Could not parse date string: {date_string}", style="red")
 
-    story_elements = page.query_selector_all('section.story-wrapper')
+    story_elements = page.query_selector_all("section.story-wrapper")
     for i, story_element in enumerate(story_elements):
         if i >= 10:
             break  # Stop after the first 10 elements
 
-        anchor = story_element.query_selector(' > a')
-        href = anchor.get_attribute('href') if anchor else ""
+        anchor = story_element.query_selector(" > a")
+        href = anchor.get_attribute("href") if anchor else ""
 
-        child_elements = story_element.query_selector_all('p') #Get all p elements.
+        child_elements = story_element.query_selector_all("p")  # Get all p elements.
         texts = [e.text_content().strip() for e in child_elements if is_valid(e.text_content())]
 
         title = texts[0] if len(texts) > 0 else ""
@@ -345,17 +400,18 @@ def parse_wsj_homepage(page) -> list[ArticleLink]:
 
     return xs
 
+
 def retrieve_nyt_home_page() -> ArticleList:
     xs = retrieve_using_playwright("https://www.nytimes.com/", parse_nytimes_homepage)
-    return ArticleList(
-        timestamp_retrieved=datetime.now().isoformat(),
-        source="New York Times",
-        articles=xs)
+    return ArticleList(timestamp_retrieved=datetime.now().isoformat(), source="New York Times", articles=list(xs.values()))
+
 
 def retrieve_nyt_home_page_ex() -> ArticleList:
     """retrieve first 10 headlines from wsj mobile home page"""
+
     def is_valid(text: str) -> bool:
         return text is not None and text != "LIVE" and "min read" not in text
+
     xs = []
     with sync_playwright() as p:
         console.print("fetching nytimes", style="yellow")
@@ -372,27 +428,27 @@ def retrieve_nyt_home_page_ex() -> ArticleList:
                 button.click()
 
             # accept compliance
-            if div := page.wait_for_selector('div#complianceOverlay', timeout=1000):
-                div.wait_for_selector('button', timeout=100).click()
+            if div := page.wait_for_selector("div#complianceOverlay", timeout=1000):
+                div.wait_for_selector("button", timeout=100).click()
 
-            if element:= page.locator('span[data-testid="todays-date"]'):
+            if element := page.locator('span[data-testid="todays-date"]'):
                 date_string = element.inner_text()
                 try:
                     # Parse the date string, ignoring the day
-                    pubdate = datetime.strptime(date_string, '%A, %B %d, %Y').date()
+                    pubdate = datetime.strptime(date_string, "%A, %B %d, %Y").date()
                     console.print(f"published {pubdate}", style="yellow")
                 except ValueError:
                     console.print(f"Error: Could not parse date string: {date_string}", style="red")
-            story_elements = page.query_selector_all('section.story-wrapper')
+            story_elements = page.query_selector_all("section.story-wrapper")
 
             for i, story_element in enumerate(story_elements):
                 if i >= 10:
                     break  # Stop after the first 10 elements
 
-                anchor = story_element.query_selector(' > a')
-                href = anchor.get_attribute('href') if anchor else ""
+                anchor = story_element.query_selector(" > a")
+                href = anchor.get_attribute("href") if anchor else ""
 
-                child_elements = story_element.query_selector_all('p') #Get all p elements.
+                child_elements = story_element.query_selector_all("p")  # Get all p elements.
                 texts = [e.text_content().strip() for e in child_elements if is_valid(e.text_content())]
 
                 title = texts[0] if len(texts) > 0 else ""
@@ -407,12 +463,13 @@ def retrieve_nyt_home_page_ex() -> ArticleList:
 
     return ArticleList(timestamp_retrieved=datetime.now().isoformat(), source="New York Times", articles=xs)
 
+
 def retrieve_wsj_home_page() -> ArticleList:
-    xs = retrieve_using_playwright("https://www-wsj-com.translate.goog/?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp&_x_tr_hist=true",                                        
-                                        parse_wsj_homepage)
+    xs = retrieve_using_playwright("https://www-wsj-com.translate.goog/?_x_tr_sl=auto&_x_tr_tl=en&_x_tr_hl=en&_x_tr_pto=wapp&_x_tr_hist=true", parse_wsj_homepage)
     return ArticleList(timestamp_retrieved=datetime.now().isoformat(), source="Wall Street Journal", articles=xs)
 
-def retrieve_wsj_home_page_ex() -> ArticleList:
+
+def retrieve_wsj_home_page_ex() -> ArticleList | None:
     """retrieve first 10 headlines from wsj mobile home page"""
     xs = []
     with sync_playwright() as p:
@@ -564,7 +621,6 @@ def retrieve_ft_article(url: str) -> str:
     if content:
         if title := text_between(content, "<title>", "</title>"):
             citation.title = title  # citation can contain abbreviated title so replace
-        # buffer = f"## {citation.title}\n\n**source:** {citation.url}\n\n**date:** {citation.date}\n\n"
 
         # the archive version of ft articles contains the extracted text as well as the html
         start = content.find("articleBody")
@@ -579,14 +635,22 @@ def retrieve_ft_article(url: str) -> str:
     return text
 
 
+@cache
+def retrieve_cached_headlines():
+    """retrieve from both sites as we need to web scrape. results are cached"""
+    return retrieve_using_playwright({NYT_URL: parse_nyt_homepage, WSJ_URL: parse_wsj_homepage})
+
+
 def retrieve_headlines(source: str = "") -> ArticleList:
     s = source.lower()
     if s == "ft":
         return merge(retrieve_ft_most_read(), retrieve_ft_most_read_section("https://www.ft.com/technology"), retrieve_ft_most_read_section("https://www.ft.com/markets"))
     if s == "nyt":
-        return retrieve_nyt_home_page()
+        xs = retrieve_cached_headlines()[NYT_URL]
+        return ArticleList(timestamp_retrieved=datetime.now().isoformat(), source="New York Times", articles=xs)
     if s == "wsj":
-        return retrieve_wsj_home_page()
+        xs = retrieve_cached_headlines()[WSJ_URL]
+        return ArticleList(timestamp_retrieved=datetime.now().isoformat(), source="Wall Street Journal", articles=xs)
     return extract_bbc_most_read()
 
 
@@ -599,24 +663,30 @@ def retrieve_article(url: str = "") -> str:
 
 
 def retrieve_stock_quotes(tickers: list[str]) -> QuoteList | None:
-    xs = []
-    with sync_playwright() as p:
-        try:
-            browser = p.chromium.launch(headless=False)
-            page = browser.new_page()
-            xs = [process_bnn_stock_page(page, add_exchange_info(e)) for e in sorted(tickers)]
-        except Exception as e:
-            console.print(f"error retrieving {e}", style="red")
-            return None
-        finally:
-            browser.close()
-    return QuoteList(timestamp_retrieved=datetime.now().isoformat(), quotes=xs)
+    "retrieve the closing price from the web page and other fields via REST calls"
+
+    def make_url(ticker: str) -> str:
+        t = ticker.upper()
+        return f"https://www.bnnbloomberg.ca/stock/{t}/" if ticker.find(":") >= 0 else f"https://www.bnnbloomberg.ca/stock/{t}:UN/"
+
+    d = {make_url(e): process_bnn_stock_page for e in tickers}
+    result = retrieve_using_playwright(d)
+    for r in result.values():
+        r.update(get_bnnbloomberg_quote(r["symbol"]))
+    return result if isinstance(result, ErrorInfo) else QuoteList(timestamp_retrieved=datetime.now().isoformat(), quotes=list(result.values()))
 
 
 if __name__ == "__main__":
-    # items = retrieve_nyt_home_page()
+    # get_bnnbloomberg_quote("JNK:UN")
+    # exit(0)
+    # pprint(retrieve_stock_quotes(["JNK", "TLT", "SPY", "PBW"]))
+    # exit(0)
+    items = retrieve_headlines("nyt")
+    print_most_read_table(items)
+
+    items = retrieve_headlines("wsj")
+    print_most_read_table(items)
     # items = retrieve_ft_most_read()
-    # print_most_read_table(items)
     # items2 = retrieve_ft_most_read_section("https://www.ft.com/markets")
     # print_most_read_table(items)
     # md = retrieve_wsj_article('https://www.wsj.com/politics/elections/democrat-party-strategy-progressive-moderates-13e8df10')
@@ -625,10 +695,10 @@ if __name__ == "__main__":
     # items = extract_bbc_most_read()
     # print_most_read_table(items)
 
-    items = retrieve_wsj_home_page()
-    print_most_read_table(items)
+    # items = retrieve_wsj_home_page()
+    # print_most_read_table(items)
     # console.print(Markdown(items.to_markdown(), style="cyan"), width=80)
     # print_most_read_table(items2)
     # m = merge(items, items2)
     # print_most_read_table(m)
-    pprint(items)
+    # pprint(items)
